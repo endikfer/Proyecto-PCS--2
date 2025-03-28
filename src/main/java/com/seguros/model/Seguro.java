462bfa278ac903c83293c6ac766c0c8c3172782c
Endika Fernandez <fernandez.endika@opendeusto.es>
2025-03-28 19:40:26 +0100

    clase lista

--- src/main/java/com/seguros/model/Seguro.java
+++ src/main/java/com/seguros/model/Seguro.java
@@ -1,12 +1,19 @@
 package com.seguros.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
 @Entity
 @Table(name = "seguros")
 public class Seguro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nombre;
 
@@ -26,6 +33,14 @@ public class Seguro {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public TipoSeguro getTipoSeguro() {
