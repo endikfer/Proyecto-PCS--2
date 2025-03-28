1564a911f823e2633fe28838ec101f0975fcb022
Alejandro <alex.pelegrin@opendeusto.es>
2025-03-28 13:41:05 +0100

    registro.html y añadido al pom.xml

--- src/main/java/com/seguros/model/Seguro.java
+++ src/main/java/com/seguros/model/Seguro.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -20,8 +22,8 @@ public class Seguro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-
+    @ManyToOne
+    @JoinColumn(name = "tipo_seguro_id")
     private TipoSeguro tipoSeguro;
 
     private String nombre;
