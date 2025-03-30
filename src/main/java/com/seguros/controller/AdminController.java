f745df0add97ef3a5a934fb23afacd1a9b90a3ef
IkerPildain <iker.pildain@opendeusto.es>
2025-03-30 15:02:42 +0200

    Inicio de sesión API

---
+++ src/main/java/com/seguros/controller/AdminController.java
@@ -0,0 +1,30 @@
+package com.seguros.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.seguros.model.Administrador;
+import com.seguros.repository.AdministradorRepository;
+
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin(origins = "*")
+public class AdminController {
+
+    @Autowired
+    private AdministradorRepository administradorRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Administrador admin) {
+        Administrador administrador = administradorRepository.findByEmail(admin.getEmail());
+        if (administrador != null && administrador.getPassword().equals(admin.getPassword())) {
+            // Autenticación exitosa
+            return ResponseEntity.ok("Inicio de sesión exitoso");
+        } else {
+            // Autenticación fallida
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
+        }
+    }
+}
