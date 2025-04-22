168002fb8060ec1bbc60ee54185aafc72ac7463d
Endika Fernandez <fernandez.endika@opendeusto.es>
2025-04-22 18:46:01 +0200

    test del main listo

---
+++ src/test/java/com/seguros/SegurosApplicationTests.java
@@ -0,0 +1,12 @@
+package com.seguros;
+
+import org.junit.jupiter.api.Test;
+
+public class SegurosApplicationTests {
+
+    @Test
+    void testMainMethod() {
+        SegurosApplication.main(new String[] {});
+    }
+
+}
