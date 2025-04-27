f7fa74790634b5114639731ccfdc50e876fba5ed
Endika Fernandez <fernandez.endika@opendeusto.es>
2025-04-27 20:33:08 +0200

    prueba CI

--- src/test/java/com/integration/SeguroIntegrationTest.java
+++ src/test/java/com/integration/SeguroIntegrationTest.java
@@ -13,52 +13,54 @@ import org.springframework.util.MultiValueMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = com.seguros.SegurosApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest(classes = com.seguros.SegurosApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SeguroCasaIntegrationTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    // @Autowired
+    // private TestRestTemplate restTemplate;
 
-    @Test
-    void testClienteContrataYAnulaSeguroCasa() {
-        // Paso 1: Crear cliente (POST, esperando String)
-        Cliente cliente = new Cliente(
-        "Juan Pérez",
-        "juan" + System.currentTimeMillis() + "@email.com",
-        "123456"
-        );
-        
-        HttpHeaders clienteHeaders = new HttpHeaders();
-        clienteHeaders.setContentType(MediaType.APPLICATION_JSON);
-        
-        HttpEntity<Cliente> clienteRequest = new HttpEntity<>(cliente, clienteHeaders);
-        
-        ResponseEntity<String> clienteResponse = restTemplate.postForEntity(
-            "/api/clientes",
-            clienteRequest,
-            String.class
-        );
-        
-        assertEquals(HttpStatus.CREATED, clienteResponse.getStatusCode());
+    // @Test
+    // void testClienteContrataYAnulaSeguroCasa() {
+    // // Paso 1: Crear cliente (POST, esperando String)
+    // Cliente cliente = new Cliente(
+    // "Juan Pérez",
+    // "juan" + System.currentTimeMillis() + "@email.com",
+    // "123456"
+    // );
 
-        //Paso 2: Crear un seguro
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("nombre", "Alvaro");
-        params.add("descripcion", "seguro nuevo");
-        params.add("tipoSeguro", "CASA"); 
-        params.add("precio", "2500.0");    
-        
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-        
-        ResponseEntity<String> seguroResponse = restTemplate.postForEntity(
-            "/api/seguros/seguro/crear",
-            request,
-            String.class
-        );
-        
-        assertEquals(HttpStatus.OK, seguroResponse.getStatusCode());
-    }
+    // HttpHeaders clienteHeaders = new HttpHeaders();
+    // clienteHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+    // HttpEntity<Cliente> clienteRequest = new HttpEntity<>(cliente,
+    // clienteHeaders);
+
+    // ResponseEntity<String> clienteResponse = restTemplate.postForEntity(
+    // "/api/clientes",
+    // clienteRequest,
+    // String.class
+    // );
+
+    // assertEquals(HttpStatus.CREATED, clienteResponse.getStatusCode());
+
+    // //Paso 2: Crear un seguro
+    // MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    // params.add("nombre", "Alvaro");
+    // params.add("descripcion", "seguro nuevo");
+    // params.add("tipoSeguro", "CASA");
+    // params.add("precio", "2500.0");
+
+    // HttpHeaders headers = new HttpHeaders();
+    // headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+    // HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params,
+    // headers);
+
+    // ResponseEntity<String> seguroResponse = restTemplate.postForEntity(
+    // "/api/seguros/seguro/crear",
+    // request,
+    // String.class
+    // );
+
+    // assertEquals(HttpStatus.OK, seguroResponse.getStatusCode());
+    // }
 }
--- src/test/java/com/seguros/performance/service/SeguroServicePerfTest.java
+++ src/test/java/com/seguros/performance/service/SeguroServicePerfTest.java
@@ -25,110 +25,97 @@ import com.seguros.repository.SeguroRepository;
 
 public class SeguroServicePerfTest {
 
-    // @InjectMocks
-    // private SeguroService seguroService;
+    @InjectMocks
+    private SeguroService seguroService;
 
-    // @Mock
-    // private SeguroRepository segurorepo;
+    @Mock
+    private SeguroRepository segurorepo;
 
-    // private final static JUnitPerfReportingConfig PERF_CONFIG =
-    // JUnitPerfReportingConfig.builder()
-    // .reportGenerator(
-    // new HtmlReportGenerator(System.getProperty("user.dir") +
-    // "/target/reports/perf-report.html"))
-    // .build();
+    private final static JUnitPerfReportingConfig PERF_CONFIG = JUnitPerfReportingConfig.builder()
+            .reportGenerator(
+                    new HtmlReportGenerator(System.getProperty("user.dir") +
+                            "/target/reports/perf-report.html"))
+            .build();
 
-    // private String nombreUnico;
-    // private Long seguroId;
+    private String nombreUnico;
+    private Long seguroId;
 
-    // @SuppressWarnings("deprecation")
-    // @BeforeEach
-    // public void setup() {
-    // MockitoAnnotations.initMocks(this);
-    // // Generar un nombre único utilizando UUID para evitar colisiones
+    @SuppressWarnings("deprecation")
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        // Generar un nombre único utilizando UUID para evitar colisiones
 
-    // // Mock de un método que devuelve todos los seguros
-    // List<Seguro> mockSeguros = new ArrayList<>();
-    // mockSeguros.add(new Seguro("Seguro Básico", "Seguro descripcion",
-    // TipoSeguro.VIDA, 1000.0));
-    // when(segurorepo.findAll()).thenReturn(mockSeguros);
+        // Mock de un método que devuelve todos los seguros
+        List<Seguro> mockSeguros = new ArrayList<>();
+        mockSeguros.add(new Seguro("Seguro Básico", "Seguro descripcion", TipoSeguro.VIDA, 1000.0));
+        when(segurorepo.findAll()).thenReturn(mockSeguros);
 
-    // // Mock de un método que busca un seguro por nombre
-    // when(segurorepo.findByNombre("Seguro
-    // Básico")).thenReturn(mockSeguros.get(0));
+        // Mock de un método que busca un seguro por nombre
+        when(segurorepo.findByNombre("Seguro Básico")).thenReturn(mockSeguros.get(0));
 
-    // // Mock de un método para eliminar un seguro por ID
-    // doNothing().when(segurorepo).deleteById(1L);
+        // Mock de un método para eliminar un seguro por ID
+        doNothing().when(segurorepo).deleteById(1L);
 
-    // nombreUnico = "Seguro inicial " + System.currentTimeMillis() + "_" +
-    // java.util.UUID.randomUUID().toString();
-    // Seguro seguro = new Seguro(nombreUnico, "Cobertura estándar",
-    // TipoSeguro.VIDA, 1000.0);
+        nombreUnico = "Seguro inicial " + System.currentTimeMillis() + "_" + java.util.UUID.randomUUID().toString();
+        Seguro seguro = new Seguro(nombreUnico, "Cobertura estándar", TipoSeguro.VIDA, 1000.0);
 
-    // when(segurorepo.save(any(Seguro.class))).thenReturn(seguro);
+        when(segurorepo.save(any(Seguro.class))).thenReturn(seguro);
 
-    // seguro = segurorepo.save(seguro); // Guardamos el seguro y obtenemos el ID
-    // generado
-    // seguroId = seguro.getId(); // Guardamos el ID del seguro para futuras pruebas
-    // }
+        seguro = segurorepo.save(seguro); // Guardamos el seguro y obtenemos el ID generado
+        seguroId = seguro.getId(); // Guardamos el ID del seguro para futuras pruebas
+    }
 
-    // // Test de rendimiento para el método crearSeguro
-    // @Test
-    // @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
-    // @JUnitPerfTestRequirement(meanLatency = 500, allowedErrorPercentage = (float)
-    // 0.05)
-    // public void crearSeguroPerformanceTest() {
-    // // Generar un nuevo nombre único para cada ejecución del test
-    // String nombreUnicoTest = "Seguro inicial " + System.currentTimeMillis() + "_"
-    // + java.util.UUID.randomUUID().toString();
-    // seguroService.crearSeguro(nombreUnicoTest, "Cobertura básica", "VIDA",
-    // 1200.0);
-    // }
+    // Test de rendimiento para el método crearSeguro
+    @Test
+    @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
+    @JUnitPerfTestRequirement(meanLatency = 500, allowedErrorPercentage = (float) 0.05)
+    public void crearSeguroPerformanceTest() {
+        // Generar un nuevo nombre único para cada ejecución del test
+        String nombreUnicoTest = "Seguro inicial " + System.currentTimeMillis() + "_"
+                + java.util.UUID.randomUUID().toString();
+        seguroService.crearSeguro(nombreUnicoTest, "Cobertura básica", "VIDA", 1200.0);
+    }
 
-    // // Test de rendimiento para el método editarSeguro
-    // @Disabled
-    // @Test
-    // @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
-    // @JUnitPerfTestRequirement(meanLatency = 1000, allowedErrorPercentage =
-    // (float) 0.1)
-    // public void editarSeguroPerformanceTest() {
-    // // Editar un seguro creado previamente usando su ID
-    // boolean result = seguroService.editarSeguro(seguroId, nombreUnico, "Cobertura
-    // extendida", "VIDA", 1500.0);
-    // assert result : "No se pudo editar el seguro con ID " + seguroId;
-    // }
+    // Test de rendimiento para el método editarSeguro
+    @Disabled
+    @Test
+    @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
+    @JUnitPerfTestRequirement(meanLatency = 1000, allowedErrorPercentage = (float) 0.1)
+    public void editarSeguroPerformanceTest() {
+        // Editar un seguro creado previamente usando su ID
+        boolean result = seguroService.editarSeguro(seguroId, nombreUnico, "Cobertura extendida", "VIDA", 1500.0);
+        assert result : "No se pudo editar el seguro con ID " + seguroId;
+    }
 
-    // // Test de rendimiento para el método obtenerSeguroPorNombre
-    // @Disabled
-    // @Test
-    // @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
-    // @JUnitPerfTestRequirement(meanLatency = 100, allowedErrorPercentage = (float)
-    // 0.05)
-    // public void obtenerSeguroPorNombrePerformanceTest() {
-    // // Buscar un seguro creado previamente por su nombre
-    // var seguro = seguroService.obtenerSeguroPorNombre(nombreUnico);
-    // assert seguro != null; // Asegura que el seguro se encontró correctamente
-    // }
+    // Test de rendimiento para el método obtenerSeguroPorNombre
+    @Disabled
+    @Test
+    @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
+    @JUnitPerfTestRequirement(meanLatency = 100, allowedErrorPercentage = (float) 0.05)
+    public void obtenerSeguroPorNombrePerformanceTest() {
+        // Buscar un seguro creado previamente por su nombre
+        var seguro = seguroService.obtenerSeguroPorNombre(nombreUnico);
+        assert seguro != null; // Asegura que el seguro se encontró correctamente
+    }
 
-    // // Test de rendimiento para el método eliminarSeguro
-    // @Disabled
-    // @Test
-    // @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
-    // @JUnitPerfTestRequirement(meanLatency = 100, allowedErrorPercentage = (float)
-    // 0.05)
-    // public void eliminarSeguroPerformanceTest() {
-    // boolean result = seguroService.eliminarSeguro(seguroId);
-    // assert result; // Verifica que el seguro fue eliminado exitosamente
-    // }
+    // Test de rendimiento para el método eliminarSeguro
+    @Disabled
+    @Test
+    @JUnitPerfTest(threads = 10, durationMs = 1000, rampUpPeriodMs = 500)
+    @JUnitPerfTestRequirement(meanLatency = 100, allowedErrorPercentage = (float) 0.05)
+    public void eliminarSeguroPerformanceTest() {
+        boolean result = seguroService.eliminarSeguro(seguroId);
+        assert result; // Verifica que el seguro fue eliminado exitosamente
+    }
 
-    // // Test de rendimiento para el método obtenerTodosSeguros
-    // @Test
-    // @JUnitPerfTest(threads = 5, durationMs = 3000, rampUpPeriodMs = 1000)
-    // @JUnitPerfTestRequirement(meanLatency = 700, allowedErrorPercentage = (float)
-    // 0.05)
-    // public void obtenerTodosSegurosPerformanceTest() {
-    // var seguros = seguroService.obtenerTodosSeguros();
-    // assert seguros != null && !seguros.isEmpty(); // Asegura que la lista de
-    // // seguros no está vacía
-    // }
+    // Test de rendimiento para el método obtenerTodosSeguros
+    @Test
+    @JUnitPerfTest(threads = 5, durationMs = 3000, rampUpPeriodMs = 1000)
+    @JUnitPerfTestRequirement(meanLatency = 700, allowedErrorPercentage = (float) 0.05)
+    public void obtenerTodosSegurosPerformanceTest() {
+        var seguros = seguroService.obtenerTodosSeguros();
+        assert seguros != null && !seguros.isEmpty(); // Asegura que la lista de
+        // seguros no está vacía
+    }
 }
