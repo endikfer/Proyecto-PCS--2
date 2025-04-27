43e60976325292095c73260cf86e7209a40e89ee
Endika Fernandez <fernandez.endika@opendeusto.es>
2025-04-27 17:10:00 +0200

    documento bd

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -16,4 +16,13 @@ CREATE TABLE IF NOT EXISTS administrador (
 
 insert into administrador (email, nombre, password) values ('admin@gmail.com', 'admin1234', '1234');
 
-select * from administrador;
+CREATE TABLE IF NOT EXISTS clientes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+insert into clientes (email, nombre, password) values ('user@gmail.com', 'user', 'user01');
+
+select * from clientes;
